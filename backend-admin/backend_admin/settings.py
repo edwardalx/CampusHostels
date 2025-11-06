90dfdb402a20edf96b1f1a1d6e04e7c37d71cebc
edwardalx <eobkwaku@gmail.com>
2025-11-06 18:02:36 +0000

    modified setting.py

--- backend-admin/backend_admin/settings.py
+++ backend-admin/backend_admin/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'replace-me-in-prod')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.72', 'campushostels.local']  # add your server IP or domain
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.72', 'hostels.bookshelfgh.duckdns.org']  # add your server IP or domain
 
 INSTALLED_APPS = [
     'django.contrib.admin',
