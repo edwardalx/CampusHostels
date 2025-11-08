e6c74ddb61f9638c3fd6c2a9b106c9a8f60c3288
edwardalx <eobkwaku@gmail.com>
2025-11-08 00:35:08 +0000

    Jenkins and setting.py

--- backend-admin/backend_admin/settings.py
+++ backend-admin/backend_admin/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'replace-me-in-prod')
 
 # Make sure DEBUG is a proper boolean   $env:DEBUG = "True"    
-DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1')    
+DEBUG = True   
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -17,14 +17,22 @@ ALLOWED_HOSTS = [
     '82.30.170.169',          # your public IP from logs
 ]
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hostel_admin',
+    'hostel_admin',   
 ]
+JAZZMIN_SETTINGS = {
+    "site_title": "Your Admin",
+    "site_header": "Your Administration",
+    "site_brand": "Your Site",
+    "welcome_sign": "Welcome to the administration",
+    "show_ui_builder": True,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
