842965d607549dc14bf24f363475e18e8b42c377
edwardalx <eobkwaku@gmail.com>
2025-11-18 21:35:38 +0000

    migrations for decimal money

---
+++ backend-api/CampusHostels.API/Infrastructure/Data/Migrations/20251118173255_UpdateMoneyTypesToDecimal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CampusHostels.API.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMoneyTypesToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
